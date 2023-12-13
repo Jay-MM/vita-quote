c3b19b8cfac917430bd09521040ff3bfcf0718d7
Jesus M <marquez.jay444@gmail.com>
2023-12-12 20:25:32 -0600

    updates html and integrates kanye & fox image APIs

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,75 @@
-const pokéForm = document.querySelector('form')
+const pokéForm = document.querySelector('form');
+const pokéInput = document.getElementById('pokéName');
+const pokéResult = document.getElementById('pokéResult');
+const foxImg = document.getElementById('fox-img');
+const kanyeSays = document.getElementById('kanye-quote');
 
 function searchPokémon(e) {
   e.preventDefault()
-
   console.log('Searching Pokémon Database...')
+  // get value of input 
+  console.log('Searching Pokémon DB for "' + pokéInput.value + '"')
+
+  const pokémonName = pokéInput.value
+
+  // fetch req to PokeAPI
+  fetch('https://pokeapi.co/api/v2/pokemon/'+ pokémonName)
+  .then(function (response) {
+    if (response.status === 200) {
+      // convert body to json
+      console.log('found "' + pokémonName + '" in DB')
+      return response.json()
+      } else if (response.status === 404) {
+        return console.error()
+      }
+    })
+    .then(function (pokemon) {
+      // console.log(pokemon)
+      // create elmnts
+      let h2 = document.createElement('h2')
+      let img = document.createElement('img')
+      // modify elmnts
+      h2.textContent = pokemon.name
+      img.src = pokemon.sprites.front_default
+      img.alt = pokemon.name
+      // append to DOM
+      pokéResult.append(h2)
+      pokéResult.append(img)
+      
+    })
 }
+
+fetch('https://api.kanye.rest')
+.then(function (response) {
+  if (response.status === 200) {
+    return response.json()
+  }else if (response.status === 404) {
+    return console.error()
+  }
+})
+.then(function(ye){
+  let h2 = document.createElement('h2')
+  h2.textContent = '"' + ye.quote + '"'
+  kanyeSays.append(h2)
+})
+
+fetch('https://randomfox.ca/floof/')
+.then(function(response){
+  if (response.status === 200) {
+    return response.json()
+  }else if (response.status === 404) {
+    return console.error()
+  }
+})
+.then(function(foxImage) {
+  let img = document.createElement('img')
+  img.src = foxImage.image
+  img.alt = 'random fox image'
+  img.classList.add('w-100')
+
+  foxImg.append(img)
+  
+})
 
 
 pokéForm.addEventListener('submit', searchPokémon)
